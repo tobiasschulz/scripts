bdea6d021f742391010aa72005f89264c6ecba66
Tobias Schulz <tobias@freehal.net>
2013-01-26 17:20:14 +0100

    anacron script added

--- install.sh
+++ install.sh
@@ -15,3 +15,5 @@ echo '#!/bin/bash' > /usr/local/bin/pull-scripts
 echo 'sudo su -c "cd '$P'; ./install.sh"' >> /usr/local/bin/pull-scripts
 chown root:root /usr/local/bin/pull-scripts
 chmod u=rwx,g=rx,o=rx /usr/local/bin/pull-scripts
+ln -f /usr/local/bin/pull-private /etc/cron.daily/pull-private
+
