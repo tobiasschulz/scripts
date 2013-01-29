b5e59d9a91a91759ec9d2455bfa7f33459280288
Tobias Schulz <tobias@freehal.net>
2013-01-29 15:19:07 +0100

    update script improved

--- update.sh
+++ update.sh
@@ -3,6 +3,8 @@
 cd $(dirname $0)
 export P=$(pwd)
 
+LC_ALL=C git reset --hard HEAD 2>&1 | grep -v "HEAD is now at"
+LC_ALL=C git clean -f -d
 LC_ALL=C git pull 2>&1 | grep -v "Current branch master is up to date" | grep -v "Already up-to-date"
 
 if [ `whoami` != root ]; then
