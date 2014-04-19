83ce6d0d80d8654383eb261abed900b704171f0c
Tobias Schulz <tobias@freehal.net>
2014-04-19 23:54:52 +0200

    some changes

--- install.sh
+++ install.sh
@@ -44,23 +44,6 @@ chmod 0755 /etc/cron.hourly/pull-scripts
 
 test -f /etc/vhost && exit 0
 
-# install packages
-
-export PURGE="bind9 sendmail-base sendmail-bin sendmail-cf sendmail-doc rmail tinc nmap"
-for pkg in $PURGE
-do
-	dpkg -l | grep " $pkg " >/dev/null 2>&1 && aptitude purge $PURGE
-done
-
-export INSTALL="update-motd landscape-common dnsmasq fail2ban lighttpd"
-for pkg in $INSTALL
-do
-	dpkg -l | grep " $pkg " >/dev/null 2>&1 || aptitude install $INSTALL
-done
-
-#test -f /usr/sbin/tincd || tinc-compile
-#test -f /usr/bin/nmap || nmap-compile
-
 # hosts availability script
 
 hosts install
@@ -79,21 +62,6 @@ cp -a $P/profile /etc/profile
 
 cp -ra $P/ssh/* /
 
-# dnsmasq config
-
-cp $P/dnsmasq.conf /etc/dnsmasq.conf
-rm -f /etc/dnsmasq.d/network-manager
-
-# lighttpd config
-
-mkdir -p /var/spool/hosts 2>/dev/null
-cat $P/lighttpd.conf | sed 's@HOSTNAME@'$(echo -n $(hostname))'.lan@gm' > /etc/lighttpd/lighttpd.conf
-
-# chroot scripts
-
-cp -ra $P/chroots/* /
-ln -sf $P/chroot-bash /usr/local/bin/cbash
-
 # magic sysrq
 
 echo "kernel.sysrq = 1" > /etc/sysctl.d/10-magic-sysrq.conf
@@ -103,9 +71,3 @@ echo "kernel.sysrq = 1" > /etc/sysctl.d/10-magic-sysrq.conf
 test -f /usr/local/bin/sensors-detect || curl http://dl.lm-sensors.org/lm-sensors/files/sensors-detect 2>/dev/null > /usr/local/bin/sensors-detect
 chmod 0755 /usr/local/bin/sensors-detect
 
-# restart services
-
-for service in dnsmasq lighttpd
-do
-	service $service restart
-done
